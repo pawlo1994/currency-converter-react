25609e5984ce8eda65a7fa449023915e344430ea
pawlo4-94 <pawlo4-94@o2.pl>
2021-06-11 19:32:59 +0200

    item with pln value added

--- src/List/index.js
+++ src/List/index.js
@@ -1,18 +1,23 @@
 import "./style.css";
 
-const List = ({ currencies, result }) => (
-    <ul className="list">
-        {currencies.map(currency => (
-            <li
-                key={currency.id}
-                className="list__item"
-            >
-                {currency.currencyName}:
-                <span className="list__span list__span--range">Kurs: {currency.currencyValue} PLN</span>
-                <span className="list__span">Wartość: {result.toFixed(2)} {currency.currencyName}</span>
+const List = ({ currencies, plnValue }) => {
+    return (
+        <ul className="list">
+            <li className="list__item">PLN:
+                <span className="list__span">Wartość: {plnValue} PLN</span>
             </li>
-        ))}
-    </ul>
-);
+            {currencies.map(currency => (
+                <li
+                    key={currency.id}
+                    className="list__item"
+                >
+                    {currency.name}:
+                    <span className="list__span list__span--range">Kurs: {currency.value} PLN</span>
+                    <span className="list__span">Wartość: {currency.result} {currency.name}</span>
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 export default List;
