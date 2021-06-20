141c260200fddbf569c2ab61fd7be0dd11eae62e
pawlo4-94 <pawlo4-94@o2.pl>
2021-06-20 11:23:18 +0200

    dateContent added, useEffect edited

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -2,33 +2,30 @@ import "./style.css";
 import { useState, useEffect } from "react";
 
 const Clock = () => {
-    const [date, setDate] = useState("");
+    const [date, setDate] = useState(new Date());
+    const dateContent = date.toLocaleString("pl", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setDate(new Date().toLocaleString("pl",
-                {
-                    weekday: "long",
-                    day: "numeric",
-                    month: "long",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    second: "2-digit"
-                }));
-        }, 0);
+            setDate(new Date());
+        }, 1000);
 
         return () => {
-            clearInterval(intervalId)
+            clearInterval(intervalId);
         };
-    });
+    }, [date]);
 
-    //setTimeout(() => {
-    //setInterval(() => {
-    //setDate(new Date());
-    //}, 1000);
-    //}, 0);
-
-    return (<p className="clock">Dzisiaj jest {date} </p>)
+    return (
+        <p className="clock">
+            Dzisiaj jest {dateContent}
+        </p>)
 };
 
 export default Clock;
